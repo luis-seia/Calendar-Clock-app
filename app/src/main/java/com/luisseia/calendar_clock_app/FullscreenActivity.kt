07a11f3ce138c562e5f934da8a5934e1409be8bc
Luis-Seia <luis.seiax@gmail.com>
2023-08-16 16:36:41 +0200

    created layout

---
+++ app/src/main/java/com/luisseia/calendar_clock_app/FullscreenActivity.kt
@@ -0,0 +1,39 @@
+package com.luisseia.calendar_clock_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.luisseia.calendar_clock_app.databinding.ActivityFullscreenBinding
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class FullscreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFullscreenBinding
+    private lateinit var fullscreenContent: TextView
+    private lateinit var fullscreenContentControls: LinearLayout
+    private val hideHandler = Handler(Looper.myLooper()!!)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFullscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
+    }
+}
--- app/src/main/java/com/luisseia/calendar_clock_app/MainActivity.kt
+++ app/src/main/java/com/luisseia/calendar_clock_app/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.luisseia.calendar_clock_app
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, FullscreenActivity::class.java))
+        },3000)
     }
 }
